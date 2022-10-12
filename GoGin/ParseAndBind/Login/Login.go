dfe9223f2eb7869e699873331ea0d60fd98e4152
XdpCs <451679250@qq.com>
2022-10-12 10:56:44 +0800

    feat(parseandbind): add parseAndbind
    add parseAndbind

---
+++ GoGin/ParseAndBind/Login/Login.go
@@ -0,0 +1,7 @@
+package Login
+
+type Login struct {
+	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+	User     string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
